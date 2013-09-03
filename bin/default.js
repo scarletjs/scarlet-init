ee22892afc7b59c75366629174a663e8dfdd283f
tjchaplin <tjchaplin@hotmail.com>
2013-09-03 08:10:43 +0100

    Updated to include a default template

---
+++ bin/default.js
@@ -0,0 +1,32 @@
+exports.name = prompt('name', package.name || basename);
+exports.license = prompt('license', "MIT");
+exports.version = prompt('version', package.version || '0.0.0');
+exports.description = prompt('description', "Awesome Scarlet Plugin");
+
+if(package.repository){
+	if(package.repository.url)
+		exports.homepage =prompt('homepage',package.repository.url);
+}
+
+package.peerDependencies = {
+    "scarlet": "~0.5.11"
+};
+
+package.devDependencies = {
+	"mocha": "*",
+    "should": "*",
+    "grunt": "~0.4.1",
+    "grunt-contrib-jshint": "~0.4.3",
+    "grunt-contrib-watch": "~0.4.0",
+    "grunt-release": "~0.3.3",
+    "scarlet": "~0.5.11"
+};
+
+package.directories = {
+    "test": "tests",
+    "lib": "lib"
+};
+
+package.scripts.test = "mocha --reporter spec tests/*";
+
+console.log(this);
